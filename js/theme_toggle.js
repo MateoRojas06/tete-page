7dfefe1a2302f262dc31f83df62259475a826fe8
MateoRojas06 <mateo.rojas@estudiante.uc.cl>
2026-07-18 10:42:28 -0400

    added persistent storage for theme (only landing page)

--- js/theme_toggle.js
+++ js/theme_toggle.js
@@ -1,7 +1,17 @@
 const toggleButton = document.getElementById('theme_toggle');
+const currentTheme = localStorage.getItem('theme');
 const body = document.body;
+
+if (currentTheme === 'light'){
+    body.classList.add('light-mode');
+}
 
 toggleButton.addEventListener('click', ()=>{
     body.classList.toggle('light-mode')
-    body.classList.toggle('dark-mode')
-})
+
+    if (body.classList.contains('light-mode')){
+        localStorage.setItem('theme', 'light');
+    } else {
+        localStorage.setItem('theme', 'dark');
+    }
+});
